1743438c5200c15a9465b0ceae45b9ca037c59c1
aytel <aytel@users.noreply.github.com>
2019-01-28 01:04:24 +0300

    added city mode

--- weather/main.py
+++ weather/main.py
@@ -4,13 +4,15 @@ import json
 import urllib.error
 
 API_KEY = 'be01cef635cf71b292eee67044cb51e3'
+#CITIES = json.load(open("./data/city.list.json", "r", encoding="utf8"))
 
 
-def getjson(cityid, API_KEY):
+def getjson(mode, city, API_KEY):
+    mode = 'q' if mode == 1 else 'id'
     response = urllib.request.urlopen(
-        "https://api.openweathermap.org/data/2.5/weather?id={}"
+        "https://api.openweathermap.org/data/2.5/weather?{}={}"
         "&units=metric"
-        "&appid={}".format(cityid, API_KEY)
+        "&appid={}".format(mode, city, API_KEY)
     )
     data = response.read().decode('ascii')
     return data
@@ -27,7 +29,13 @@ def printweather(data):
 
 
 if __name__ == '__main__':
-    cityid = sys.argv[1]
-    data = getjson(cityid, API_KEY)
+    if sys.argv[1] == 'id':
+        cityid = sys.argv[2]
+        data = getjson(0, cityid, API_KEY)
+    elif sys.argv[1] == 'city':
+        city = ' '.join(sys.argv[2:])
+        data = getjson(1, city, API_KEY)
+    else:
+        print('unknown mode')
+        exit(0)
     printweather(data)
-    # print(data)
