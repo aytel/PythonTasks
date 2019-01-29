d85fdaff60ae79c0b70b024f507b9f7f96416712
aytel <aytel@users.noreply.github.com>
2019-01-30 00:36:18 +0300

    rollback

--- weather/citygetter.py
+++ weather/citygetter.py
@@ -3,8 +3,8 @@ from jsongetter import JSONGetter
 
 class CityGetter:
     def __init__(self, city_url):
-        self._getter = JSONGetter(city_url)
+        self._getter = JSONGetter(city_url, {})
         self._CITY_URL = city_url
 
     def get(self):
-        return self._getter.get()
+        return self._getter.get({})
